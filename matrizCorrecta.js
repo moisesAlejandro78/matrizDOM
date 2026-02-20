9b467d54e681e38a921785a96e31c49533adde7c
Moises Alejandro <moisesalejandro052@gmail.com>
2026-02-19 22:46:26 -0400

    Estoy en un 90% de terminar el ejercicio solo me falta configurar el boton Resetear

--- matrizCorrecta.js
+++ matrizCorrecta.js
@@ -2,13 +2,15 @@
 let btnValidar = document.getElementById("btnValidar");
 let parrafoMatriz = document.getElementById("matriz");
 let btnReset = document.getElementById("btnResetear");
-let alerta=document.getElementById("txtAlerta");
-let matrizPIP=document.getElementById("MatrizPIP")
+let alerta = document.getElementById("txtAlerta");
+let matrizPIP = document.getElementById("MatrizPIP");
 btnValidar.addEventListener("click", function validar() {
     let num1 = parseFloat(document.getElementById("numero1").value);
     let num2 = parseFloat(document.getElementById("numero2").value);
-    let matrizUnitaria=document.getElementById("matrizUnitaria");
-
+    let matrizUnitaria = document.getElementById("matrizUnitaria");
+    let matrizPares = document.getElementById("matrizPares");
+    let matrizPrimos = document.getElementById("matrizPrimos");
+    let matrizImpares=document.getElementById("matrizImpares");
     let matriz = [];
     if (num1 == Math.abs(Math.trunc(num1)) && num2 == Math.abs(Math.trunc(num2))) {
         if (num1 == num2) {
@@ -18,27 +20,89 @@ btnValidar.addEventListener("click", function validar() {
                     matriz[index][index2] = 0;
                 }
             }//lleno de ceros la matriz
-            console.log(matriz);
+            //console.log(matriz);
             verMatriz(matriz);
-            matrizUnitaria.addEventListener("change",function matrizUnitaria() {
-                let matrizUnitaria=[];
+            matrizUnitaria.addEventListener("change", function matrizUnitaria() {
+                let matrizUnitaria = [];
                 for (let index = 0; index < num1; index++) {
-                    matrizUnitaria[index]=[];
+                    matrizUnitaria[index] = [];
                     for (let index2 = 0; index2 < num2; index2++) {
-                        if (index==index2) {
-                            matrizUnitaria[index][index2]=1
+                        if (index == index2) {
+                            matrizUnitaria[index][index2] = 1
                         }
-                        else{
-                            matrizUnitaria[index][index2]=0;
+                        else {
+                            matrizUnitaria[index][index2] = 0;
                         }
                     }
                 }
+                console.table(matrizUnitaria);
                 verMatriz(matrizUnitaria);
             });//matrizunitaria
+            matrizPares.addEventListener("change", function matrizPares() {
+                let matrizPares = [];
+                let sumaIndix = 0;
+                for (let index = 0; index < num1; index++) {//filas
+                    matrizPares[index] = [];
+                    for (let index2 = 0; index2 < num2; index2++) {//columnas
+                        sumaIndix = index + index2;//0+0=0;0+1=1...
+                        matrizPares[index][index2] = 0;
+                        if (sumaIndix % 2 == 0) {
+                            matrizPares[0][0] = 0;//aqui lo podria mejorar
+                            matrizPares[index][index2] = Math.trunc(Math.random() * 11) + 1;
+                        }
+                    }//segundo for
+                }//primer for
+                console.table(matrizPares);
+                verMatriz(matrizPares);
+            });//matrizPares
+            matrizImpares.addEventListener("change", function matrizImpares() {
+                let matrizImpares=[], sumaImpares;
+                for (let index = 0; index < num1; index++) {
+                    matrizImpares[index]=[];
+                    for (let index2 = 0; index2 < num2; index2++) {
+                        sumaImpares=index+index2;
+                        if (sumaImpares%2===0) {
+                            matrizImpares[index][index2]=0
+                        }
+                        else{
+                            matrizImpares[index][index2]=Math.trunc(Math.random()*10)+1;
+                        }
+                    }
+                }
+                console.table(matrizImpares);
+                verMatriz(matrizImpares);
+            });
+            matrizPrimos.addEventListener("change", function matrizPrimos() {
+                let matrizPrimos = [];
+                let sumaPrimos = 0;
+                for (let index = 0; index < num1; index++) {
+                    matrizPrimos[index] = [];
+                    for (let index2 = 0; index2 < num2; index2++) {
+                        let contador = 0;
+                        let aumento = 1;
+                        sumaPrimos = index + index2;
+                        matrizPrimos[index][index2] = 0;
+                        while (aumento <= sumaPrimos) {//1<6; 2<6;3<6;4<6;5<6;6<=6;7<6
+                            if ((sumaPrimos % aumento) === 0) {
+                                contador++;
+                                if (contador === 2) {
+                                    matrizPrimos[index][index2] = Math.trunc(Math.random() * 10) + 1;
+                                }
+                                else{
+                                    matrizPrimos[index][index2]=0;
+                                }
+                            }
+                            aumento++;
+                        }
+                    }//segundo for
+                }//primer for
+                console.table(matrizPrimos);
+                verMatriz(matrizPrimos);
+            });
         }//if num1==num2
-        else{
-            alerta.style.color="red";
-            alerta.textContent="los valores no son matriz cuadratica";
+        else {
+            alerta.style.color = "red";
+            alerta.textContent = "los valores no son matriz cuadratica";
         }
     }//if para validar que sean enteros positivos
     else if (num1 != Math.abs(Math.trunc(num1))) {
---
+++ sumaRestaMatrices.js
@@ -0,0 +1,10 @@
+/* let num1=parseFloat(document.getElementById("numero1").value);
+let num2=parseFloat(document.getElementById("numero2").value); */
+let num1=6,num2=6;
+let modulo=num1%num2;
+let div=num1/num2;
+
+console.log("Division");
+console.log(div);
+console.log("Modulo");
+console.log(modulo);
